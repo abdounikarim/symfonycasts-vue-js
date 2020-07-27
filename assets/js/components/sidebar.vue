5ca3a1c4a4bbac6c342c1b09627dd9dabffd4b3e
abdounikarim <abdounikarim@gmail.com>
2020-07-27 12:32:58 +0200

    Looping with v-for

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -24,17 +24,15 @@
                     href="/"
                 >All Products</a>
             </li>
-            <li class="nav-item">
+            <li
+                v-for="(category, index) in categories"
+                :key="index"
+                class="nav-item"
+            >
                 <a
                     class="nav-link"
-                    href="#"
-                >Category A</a>
-            </li>
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="#"
-                >Category B</a>
+                    :href="category.link"
+                >{{ category.name }}</a>
             </li>
         </ul>
     </div>
@@ -43,5 +41,19 @@
 <script>
 export default {
     name: 'Sidebar',
+    data() {
+        return {
+            categories: [
+                {
+                    name: 'Dot Matrix Printers',
+                    link: '#',
+                },
+                {
+                    name: 'Iomega Zip Drives',
+                    link: '#',
+                },
+            ],
+        };
+    },
 };
 </script>
