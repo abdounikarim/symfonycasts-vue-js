d41038fe54ad7424560f6e3f9a3a48a6b000997d
abdounikarim <abdounikarim@gmail.com>
2020-07-27 16:19:04 +0200

    Computed Properties

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -18,12 +18,7 @@
 
 <template>
     <div
-        :class="{
-            [$style.component]: true,
-            [$style.collapsed]: collapsed,
-            'mb-5': true,
-            'p-3': true,
-        }"
+        :class="componentClass"
     >
         <div v-show="!collapsed">
             <h5 class="text-center">
@@ -79,6 +74,22 @@ export default {
                 },
             ],
         };
+    },
+    computed: {
+        /**
+         * Computes the component classes depending on collapsed state
+         *
+         * @returns string[]
+         */
+        componentClass() {
+            const classes = [this.$style.component, 'p-3', 'mb-5'];
+
+            if (this.collapsed) {
+                classes.push(this.$style.collapsed);
+            }
+
+            return classes;
+        },
     },
     methods: {
         toggleCollapsed() {
